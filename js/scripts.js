c9e2949eab05624289b7e93cebe8279c0ca06c43
EstherKimani <esther19kimani@gmail.com>
2022-03-12 15:29:44 +0300

    calculated the day ofthe week a user is born

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
-function my-akan-name(){
+function fetchAkanName(){
+
   //declare all the names
   let edays = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   let mDays = ['Kwasi', 'Kwadwo','Kwabena','Kwaku','Yaw', 'Kofi', 'Kwame'];
   let fDays = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua', 'Ama'];
 
+
 //pass values from html to javascript
 var date = document.getElementById("date").value;
   var month = document.getElementById("month").value;
   var year = document.getElementById("year").value;
+
 //convert the user input to integers
 var DD = parseInt(date);
 var MM = parseInt(month);
@@ -17,6 +20,9 @@ var CC = Math.ceil(C/100);//rounds the number to the nearest integers
 //Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 //substitute values
 var day = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-alert("so far so good");
+var dayOfWeek = Math.ceil(day);
+alert(dayOfWeek);
 
-}
+
+
+};
