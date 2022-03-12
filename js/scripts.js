a8d1755eef0ed8fcbfe2ec6a1c1361747a71dfd8
EstherKimani <esther19kimani@gmail.com>
2022-03-12 17:03:42 +0300

    added conditions to asign the akan names

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,7 @@ var CC = Math.ceil(C/100);//rounds the number to the nearest integers
 //substitute values
 var day = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
 var dayOfWeek = Math.ceil(day);
-alert(dayOfWeek);
+
 //condition with the Gender
 if(document.getElementById('Gender').checked) {
   //Male radio button is checked
@@ -31,8 +31,67 @@ if(document.getElementById('Gender').checked) {
   var gender = "female";
 }
 
-//to test the conditions that assign the akan names
+//to test the conditions that assign the akan names(validation)
+if (DD <=0|| DD >31){
+  alert("Enter a valid date!");
+} else if(MM <= 0 || MM > 12)){
+  alert("Enter a valid date!");
+}
 
 
+  else if (calculatedDate == 0 && gender === 'male') {
+  alert(document.getElementById("display").innerHTML = ("You were born on " + days[0] + " your Akan Name is " + maleNames[0]));
+  }
+  else if (calculatedDate == 1 || intergeredDate == -1 && gender === 'male') {
+    alert(document.getElementById("display").innerHTML = ("You were born on " + days[1] + "your Akan Name is " + maleNames[1]));
+  }
+  else if (calculatedDate == 2 || intergeredDate == -2 && gender === 'male') {
+    alert(document.getElementById("display").innerHTML = ("You were born on" + days[2] + "your Akan Name is " + maleNames[2]));
+  }
+  else if (calculatedDate == 3 || intergeredDate == -3 && gender === 'male') {
+    alert(document.getElementById("display").innerHTML = ("You were born on" + days[3] + "your Akan Name is " + maleNames[3]));
+  }
+  else if (calculatedDate == 4 || intergeredDate == -4 && gender === 'male') {
+    alert(document.getElementById("display").innerHTML = ("You were born on" + days[4] + "your Akan Name is " + maleNames[4]));
+  }
+  else if (calculatedDate == 5 || intergeredDate == -5 && gender === 'male') {
+    alert(document.getElementById("display").innerHTML = ("You were born on" + days[5] + "your Akan Name is " + maleNames[5]));
+  }
+  else if (calculatedDate == 6 || intergeredDate == -6 && gender === 'male') {
+    alert(document.getElementById("display").innerHTML = ("You were born on" + days[6] + "your Akan Name is " + maleNames[6]));
+  }
+  else if (calculatedDate == 0 && gender === 'female') {
+  alert(document.getElementById("display").innerHTML = ("You were born on" + days[0] + "your Akan name is " + femaleNames[0]));
+  }
+  else if (calculatedDate == 1 || intergeredDate == -1 && gender === 'female') {
+    alert(document.getElementById("display").innerHTML = ("You were born on" + days[1] + "your Akan Name is " + femaleNames[1]));
+  }
+  else if (calculatedDate == 2 || intergeredDate == -2 && gender === 'female') {
+    alert(document.getElementById("display").innerHTML = ("You were born on" + days[2] + "your Akan Name is " + femaleNames[2]));
+  }
+  else if (calculatedDate == 3 || intergeredDate == -3 && gender === 'female') {
+    alert(document.getElementById("display").innerHTML = ("You were born on" + days[3] + "your Akan Name is " + femaleNames[3]));
+  }
+  else if (calculatedDate == 4 || intergeredDate == -4 && gender === 'female') {
+    alert(document.getElementById("display").innerHTML = ("You were born on" + days[4] + "your Akan Name is " + femaleNames[4]));
+  }
+  else if (calculatedDate == 5 || intergeredDate == -5 && gender === 'female') {
+    alert(document.getElementById("display").innerHTML = ("You were born on" + days[5] + "your Akan Name is " + femaleNames[5]));
+  }
+  else if (calculatedDate == 6 || intergeredDate == -6 && gender === 'female') {
+    alert(document.getElementById("display").innerHTML = ("You were born on" + days[6] + "your Akan Name is " + femaleNames[6]));
+  }
+  else {
+    alert("Check if you inputted the correct data!");
+  }
 
 };
+
+
+  //else if(MM <=0 || MM > 12) {
+    //alert("Enter a valid month");
+  //} else {
+  //  alert("GOOD");
+//  }
+  //else if(YY <1900 || YY>2022){
+  //  alert("Enter a valid year");
