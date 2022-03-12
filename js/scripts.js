f907f6c0ad60e5f092002c581b1f1ed734199958
EstherKimani <esther19kimani@gmail.com>
2022-03-12 14:32:18 +0300

    Declared all the variables needed in the formula converted to integers

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function my-akan-name(){
+  //declare all the names
+  let edays = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  let mDays = ['Kwasi', 'Kwadwo','Kwabena','Kwaku','Yaw', 'Kofi', 'Kwame'];
+  let fDays = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua', 'Ama'];
+
+//pass values from html to javascript
+var date = document.getElementById("date").value;
+  var month = document.getElementById("month").value;
+  var year = document.getElementById("year").value;
+//convert the user input to integers
+var DD = parseInt(date);
+var MM = parseInt(month);
+var YY = parseInt(year.toString().slice(2, 4));
+var C = parseInt(year);
+var CC = Math.ceil(C/100);//rounds the number to the nearest integers
+//Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+//substitute values
+var day = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+
+}
